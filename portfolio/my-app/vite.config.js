2434aaf5e1ca435e39455acb72bb2d178eb818f8
Mark <markberio30@gmail.com>
2026-05-11 18:47:04 +0800

    Update vite.config.js

--- portfolio/my-app/vite.config.js
+++ portfolio/my-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Marksite/' // <-- IMPORTANT for GitHub Pages
+  base: '/BerSite/' // <-- IMPORTANT for GitHub Pages
 })
