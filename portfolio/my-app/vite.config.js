b93e024d46a19bfe5d55bfc1fe368559021d63d3
Mark <markberio30@gmail.com>
2026-05-11 19:34:58 +0800

    update
    up

--- portfolio/my-app/vite.config.js
+++ portfolio/my-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BerSite/' // <-- IMPORTANT for GitHub Pages
+  base: '/', // important for Vercel root deployment
 })
