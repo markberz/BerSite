7dacf825a9f35af796a5a74cc20f00989f04f2b1
Mark <markberio30@gmail.com>
2026-05-12 19:49:51 +0800

    Update vite.config.js

--- portfolio/my-app/vite.config.js
+++ portfolio/my-app/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { cwd } from 'process'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // important for Vercel root deployment
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, cwd(), '')
+  return {
+    plugins: [react()],
+    base: env.VITE_APP_BASE_PATH || '/',
+  }
 })
